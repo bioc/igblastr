dd88df56090fc2d003ac6c7d4664a5f4ef9965e6
Hervé Pagès <hpages.on.github@gmail.com>
2025-04-29 23:18:34 -0700

    small tweak

--- tests/testthat/test-igblastn.R
+++ tests/testthat/test-igblastn.R
@@ -21,26 +21,30 @@ test_that("igblastn()", {
     out5 <- igblastn(head(query, n=10L), num_threads=5)
     expect_identical(out5, out0)
 
-    ## Call igblastn() on first 10 sequences in parallel using 4 workers.
-    library(parallel)
-    res <- mclapply(1:10, function(i) igblastn(query[i]), mc.cores=4)
-    for (i in 1:10) {
-        out_i <- res[[i]]
-        expect_true(is_tibble(out_i))
-        expect_identical(dim(out_i), c(1L, 111L))
-        expect_identical(colnames(out_i), colnames(out0))
-    }
-    expect_identical(do.call(rbind, res), out0)
+    ## mclapply() with 'mc.cores' > 1 is not supported on Windows.
+    if (.Platform$OS.type != "windows") {
+        ## Call igblastn() on first 10 sequences in parallel using 4 workers.
+        library(parallel)
+        res <- mclapply(1:10, function(i) igblastn(query[i]), mc.cores=4)
+        for (i in 1:10) {
+            out_i <- res[[i]]
+            expect_true(is_tibble(out_i))
+            expect_identical(dim(out_i), c(1L, 111L))
+            expect_identical(colnames(out_i), colnames(out0))
+        }
+        expect_identical(do.call(rbind, res), out0)
 
-    ## Call igblastn() on first 5 sequences in parallel using 2 workers
-    ## that try to write to the same output file.
-    out <- tempfile()
-    expect_warning(
-      res <- mclapply(1:5, function(i) igblastn(query[i], out=out), mc.cores=2),
-      regexp="all scheduled cores encountered errors in user code"
-    )
-    is_error <- vapply(res, function(out) inherits(out, "try-error"),
-                       logical(1))
-    expect_true(all(is_error))
+        ## Call igblastn() on first 5 sequences in parallel using 2 workers
+        ## that try to write to the same output file.
+        out <- tempfile()
+        expect_warning(
+            res <- mclapply(1:5, function(i) igblastn(query[i], out=out),
+                            mc.cores=2),
+            regexp="all scheduled cores encountered errors in user code"
+        )
+        is_error <- vapply(res, function(out) inherits(out, "try-error"),
+                           logical(1))
+        expect_true(all(is_error))
+    }
 })
 
