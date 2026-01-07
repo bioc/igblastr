46e99577a97ff874a1ef0768696c41e53989005b
Hervé Pagès <hpages.on.github@gmail.com>
2026-01-07 13:14:46 -0800

    Add annotate_heavy_V_alleles() and annotate_light_V_alleles()

--- R/intdata-utils.R
+++ R/intdata-utils.R
@@ -4,6 +4,13 @@
 ###
 
 
+### List only the FWR/CDR regions that are fully part of the V region.
+.FWR_CDR_REGIONS <- c(paste0(c("fwr", "cdr"), rep(1:2, each=2)), "fwr3")
+
+.FWR_CDR_START_END_COLNAMES <- paste0(rep(.FWR_CDR_REGIONS, each=2L),
+                                      c("_start", "_end"))
+
+
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### get_intdata_path()
 ###
@@ -145,9 +152,8 @@ load_intdata <- function(organism, for.aa=FALSE,
 ### or 'intdata$<region>_end' has an NA.
 .translate_V_region <- function(V_alleles, intdata, region)
 {
-    valid_regions <- c(paste0(c("fwr", "cdr"), rep(1:2, each=2)), "fwr3")
-    if (!(isSingleNonWhiteString(region) && (region %in% valid_regions))) {
-        in1string <- paste0("\"", valid_regions, "\"", collapse=", ")
+    if (!(isSingleNonWhiteString(region) && (region %in% .FWR_CDR_REGIONS))) {
+        in1string <- paste0("\"", .FWR_CDR_REGIONS, "\"", collapse=", ")
         stop(wmsg("'region' must be one of ", in1string))
     }
     start_colname <- paste0(region, "_start")
@@ -179,5 +185,159 @@ V_allele_has_stop_codon <- function(V_alleles, intdata)
     ans <- setNames(grepl("*", V_aa, fixed=TRUE), names(V_aa))
     ans[is.na(V_aa)] <- NA
     ans
+}
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### annotate_heavy_V_alleles()
+### annotate_light_V_alleles()
+###
+### EXPERIMENTAL!
+
+.load_J_alleles <- function(db_name, loci)
+{
+    stop_if_malformed_loci_vector(loci)
+    db_path <- make_germline_db_path(db_name)
+    fasta_files <- get_original_db_fasta_files(db_path, "J")
+    found_loci <- substr(basename(fasta_files), 1L, 3L)
+    stopifnot(all(loci %in% found_loci))
+    fasta_files <- fasta_files[found_loci %in% loci]
+
+    ## Combine and edit FASTA files (see .combine_and_edit_fasta_files()
+    ## in file create_region_db.R).
+    combined_fasta <- tempfile()
+    on.exit(unlink(combined_fasta))
+    concatenate_files(fasta_files, combined_fasta)
+    final_fasta <- tempfile()
+    on.exit(unlink(final_fasta))
+    redit_imgt_file(combined_fasta, final_fasta)
+
+    readDNAStringSet(final_fasta)
+}
+
+.assemble_artificial_heavy_chains <- function(V_alleles, D_alleles, J_alleles)
+{
+    stopifnot(is(V_alleles, "DNAStringSet"),
+              is(D_alleles, "DNAStringSet"),
+              is(J_alleles, "DNAStringSet"))
+    V_names <- names(V_alleles)
+    D_names <- names(D_alleles)
+    J_names <- names(J_alleles)
+    stopifnot(!is.null(V_names), !is.null(D_names), !is.null(J_names))
+    ans <- xscat(rep(V_alleles, each=length(D_alleles)), D_alleles)
+    ans_names <- paste0(rep(V_names, each=length(D_alleles)), "+", D_names)
+    ans <- xscat(rep(ans, each=length(J_alleles)), J_alleles)
+    ans_names <- paste0(rep(ans_names, each=length(J_alleles)), "+", J_names)
+    setNames(ans, ans_names)
+}
+
+.assemble_artificial_light_chains <- function(V_alleles, J_alleles)
+{
+    stopifnot(is(V_alleles, "DNAStringSet"),
+              is(J_alleles, "DNAStringSet"))
+    V_names <- names(V_alleles)
+    J_names <- names(J_alleles)
+    stopifnot(!is.null(V_names), !is.null(J_names))
+    ans <- xscat(rep(V_alleles, each=length(J_alleles)), J_alleles)
+    ans_names <- paste0(rep(V_names, each=length(J_alleles)), "+", J_names)
+    setNames(ans, ans_names)
+}
+
+### Returns a 3-col matrix with 1 row per name in 'nms'.
+.split_artificial_heavy_names <- function(nms)
+{
+    stopifnot(is.character(nms))
+    parts <- strsplit(nms, "+", fixed=TRUE)
+    stopifnot(all(lengths(parts) == 3L))
+    data <- if (length(parts) == 0L) character(0) else unlist(parts)
+    matrix(data, ncol=3L, byrow=TRUE,
+           dimnames=list(NULL, c("Vpart", "Dpart", "Jpart")))
+}
+
+### Returns a 2-col matrix with 1 row per name in 'nms'.
+.split_artificial_light_names <- function(nms)
+{
+    stopifnot(is.character(nms))
+    parts <- strsplit(nms, "+", fixed=TRUE)
+    stopifnot(all(lengths(parts) == 2L))
+    data <- if (length(parts) == 0L) character(0) else unlist(parts)
+    matrix(data, ncol=2L, byrow=TRUE,
+           dimnames=list(NULL, c("Vpart", "Jpart")))
+}
+
+.annotate_artificial <- function(artificial, db_name)
+{
+    prev_db_name <- try(use_germline_db(), silent=TRUE)
+    if (!inherits(prev_db_name, "try-error"))
+        on.exit(suppressMessages(use_germline_db(prev_db_name)))
+    suppressMessages(use_germline_db(db_name))
+    AIRR_df <- igblastn(artificial, num_alignments_V=1,
+                                    num_alignments_D=1,
+                                    num_alignments_J=1)
+    ans_colnames <- c("locus", "v_call", "d_call", "j_call",
+                      .FWR_CDR_START_END_COLNAMES)
+    AIRR_df[ , ans_colnames]
+}
+
+.drop_rows_with_wrong_locus <- function(df0, locus)
+{
+    stopifnot(is.data.frame(df0), isSingleNonWhiteString(locus))
+    ok <- df0[ , "locus"] %in% locus
+    if (all(ok))
+        return(df0)
+    bad_df <- df0[!ok, ]
+    warning("Dropping the following rows (bad locus):", immediate.=TRUE)
+    print(bad_df)
+    df0[ok, ]
+}
+
+.extract_intdata_from_df0 <- function(df0, V_names)
+{
+    stopifnot(is.data.frame(df0))
+    f <- factor(df0[ , "Vpart"], levels=V_names)
+    stopifnot(!anyNA(f))
+    X <- setNames(.FWR_CDR_START_END_COLNAMES, .FWR_CDR_START_END_COLNAMES)
+    intdata <- lapply(X,
+        function(colname) {
+            tmp <- unique(splitAsList(df0[ , colname], f))
+            stopifnot(identical(names(tmp), V_names))
+            bad_idx <- which(lengths(tmp) != 1L)
+            if (length(bad_idx) != 0L)
+                tmp[bad_idx] <- NA_integer_
+            as.integer(tmp)
+        })
+    as.data.frame(c(list(allele_name=V_names), intdata),
+                  optional=TRUE, fix.empty.names=FALSE)
+}
+
+### Returns a data.frame with 1 row per sequence in 'V_alleles'.
+### The _start/_end columns can contain NAs.
+annotate_heavy_V_alleles <- function(V_alleles, db_name)
+{
+    D_alleles <- load_germline_db(db_name, region_types="D")
+    J_alleles <- .load_J_alleles(db_name, "IGH")
+    artificial <- .assemble_artificial_heavy_chains(V_alleles,
+                                                    D_alleles,
+                                                    J_alleles)
+    name_parts <- .split_artificial_heavy_names(names(artificial))
+    df0 <- cbind(name_parts, .annotate_artificial(artificial, db_name))
+    df0 <- .drop_rows_with_wrong_locus(df0, "IGH")
+    .extract_intdata_from_df0(df0, names(V_alleles))
+}
+
+### Returns a data.frame with 1 row per sequence in 'V_alleles'.
+### The _start/_end columns can contain NAs.
+annotate_light_V_alleles <- function(V_alleles, db_name, locus)
+{
+    if (!isSingleNonWhiteString(locus))
+        stop(wmsg("'locus' must be a single (non-empty) string"))
+    if (!(locus %in% c("IGK", "IGL")))
+        stop(wmsg("'locus' must be \"IGK\" or \"IGL\""))
+    J_alleles <- .load_J_alleles(db_name, locus)
+    artificial <- .assemble_artificial_light_chains(V_alleles, J_alleles)
+    name_parts <- .split_artificial_light_names(names(artificial))
+    df0 <- cbind(name_parts, .annotate_artificial(artificial, db_name))
+    df0 <- .drop_rows_with_wrong_locus(df0, locus)
+    .extract_intdata_from_df0(df0, names(V_alleles))
 }
 
