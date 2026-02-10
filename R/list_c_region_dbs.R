9051e4ce2ead31ad1a9e418dc667d2a8b8174148
Hervé Pagès <hpages.on.github@gmail.com>
2026-02-09 19:30:26 -0800

    move use_germline_db() and use_c_region_db() to their own .R/.Rd files

--- R/list_c_region_dbs.R
+++ R/list_c_region_dbs.R
@@ -47,6 +47,7 @@
     !.has_builtin_IMGT_TR_c_region_dbs(c_region_dbs_home)
 }
 
+### Not exported!
 ### Returns path to C_REGION_DBS cache compartment (see R/cache-utils.R for
 ### details about igblastr's cache organization).
 ### When 'init.path=TRUE':
@@ -91,7 +92,7 @@ print.c_region_dbs_df <- function(x, ...)
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### .check_c_region_db_name()
+### check_c_region_db_name()
 ###
 
 .stop_on_invalid_c_region_db_name <- function(db_name)
@@ -102,7 +103,8 @@ print.c_region_dbs_df <- function(x, ...)
     stop(wmsg(msg1), "\n  ", wmsg(msg2))
 }
 
-.check_c_region_db_name <- function(db_name)
+### Not exported!
+check_c_region_db_name <- function(db_name)
 {
     if (!isSingleNonWhiteString(db_name))
         stop(wmsg("'db_name' must be a single (non-empty) string"))
@@ -113,12 +115,12 @@ print.c_region_dbs_df <- function(x, ...)
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### c_region_db_path()
+### get_c_region_db_path()
 ###
 
 ### Not exported!
 ### Note that the returned path is NOT guaranteed to exist.
-c_region_db_path <- function(db_name)
+get_c_region_db_path <- function(db_name)
 {
     if (!isSingleNonWhiteString(db_name))
         stop(wmsg("'db_name' must be a single (non-empty) string"))
@@ -129,102 +131,12 @@ c_region_db_path <- function(db_name)
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### use_c_region_db()
-###
-
-### Returns "" if no db is currently in use.
-.get_c_region_db_in_use <- function(verbose=FALSE)
-{
-    c_region_dbs_home <- get_c_region_dbs_home(TRUE)  # guaranteed to exist
-    db_path <- get_db_in_use(c_region_dbs_home, what="C-region")
-    if (db_path == "")
-        return(db_path)
-    make_blastdbs(db_path, verbose=verbose)
-    basename(db_path)
-}
-
-.how_to_suppress_use_c_region_db_msg <- function(db_name)
-{
-    msg1 <- "To suppress this message, use:"
-    msg2 <- c("suppressMessages(use_c_region_db(\"", db_name, "\"))")
-    c(wmsg(msg1), "\n    ", wmsg(msg2))
-}
-
-.note_on_selecting_IMGT_c_region_db <- function(db_name)
-{
-    is_imgt_db <- grepl("^_?IMGT\\.", db_name)
-    if (!is_imgt_db)
-        return()
-    message("  ", wmsg(IMGT_TERMS_OF_USE), "\n\n  ",
-            .how_to_suppress_use_c_region_db_msg(db_name))
-}
-
-.select_c_region_db <- function(db_name, verbose=FALSE)
-{
-    if (!isSingleString(db_name))
-        stop(wmsg("'db_name' must be a single string"))
-
-    if (db_name != "") {
-        .check_c_region_db_name(db_name)
-        .note_on_selecting_IMGT_c_region_db(db_name)
-        db_path <- c_region_db_path(db_name)
-        make_blastdbs(db_path, verbose=verbose)
-    }
-
-    ## Returns 'db_name' invisibly.
-    set_db_in_use("C-region", db_name, verbose=verbose)
-}
-
-### Passing 'db_name=""' will cancel the current selection.
-use_c_region_db <- function(db_name=NULL, verbose=FALSE)
-{
-    if (!isTRUEorFALSE(verbose))
-        stop(wmsg("'verbose' must be TRUE or FALSE"))
-    if (is.null(db_name))
-        return(.get_c_region_db_in_use(verbose=verbose))
-    .select_c_region_db(db_name, verbose=verbose)
-}
-
-
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### load_c_region_db()
-###
-
-### Returns the C regions in a DNAStringSet object.
-load_c_region_db <- function(db_name)
-{
-    .check_c_region_db_name(db_name)
-    db_path <- c_region_db_path(db_name)
-    db_fasta_file <- get_db_fasta_file(db_path, "C")
-    readDNAStringSet(db_fasta_file)
-}
-
-
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### clean_c_region_blastdbs()
-###
-
-### Not used at the moment and not exported!
-clean_c_region_blastdbs <- function()
-{
-    c_region_dbs_home <- get_c_region_dbs_home()  # NOT guaranteed to exist
-    if (dir.exists(c_region_dbs_home)) {
-        all_db_names <- list_c_region_dbs(names.only=TRUE)
-        for (db_name in all_db_names) {
-            db_path <- c_region_db_path(db_name)
-            clean_blastdbs(db_path)
-        }
-    }
-}
-
-
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### rm_c_region_db()
 ###
 
 rm_c_region_db <- function(db_name)
 {
-    .check_c_region_db_name(db_name)
+    check_c_region_db_name(db_name)
     if (has_prefix(db_name, "_"))
         stop(wmsg("cannot remove a built-in C-region db"))
 
@@ -233,7 +145,7 @@ rm_c_region_db <- function(db_name)
     if (db_in_use_path != "" && basename(db_in_use_path) == db_name)
         set_db_in_use("C-region", "")  # cancel current selection
 
-    db_path <- c_region_db_path(db_name)
+    db_path <- get_c_region_db_path(db_name)
     nuke_file(db_path)
 }
 
---
+++ tests/testthat/test-use_germline_db.R
@@ -0,0 +1,25 @@
+test_that("use_germline_db()", {
+    db_name <- "_AIRR.human.IGH+IGK+IGL.202410"
+    use_germline_db(db_name)
+    expect_identical(use_germline_db(), db_name)
+})
+
+test_that("load_germline_db()", {
+    db_name <- "_AIRR.human.IGH+IGK+IGL.202410"
+    object <- load_germline_db(db_name)
+    expect_true(is(object, "DNAStringSet"))
+    expect_equal(length(object), 396)
+    object <- load_germline_db(db_name, region_types="V")
+    expect_true(is(object, "DNAStringSet"))
+    expect_equal(length(object), 342)
+    object <- load_germline_db(db_name, region_types="D")
+    expect_true(is(object, "DNAStringSet"))
+    expect_equal(length(object), 31)
+    object <- load_germline_db(db_name, region_types="J")
+    expect_true(is(object, "DNAStringSet"))
+    expect_equal(length(object), 23)
+    object <- load_germline_db(db_name, region_types="JV")
+    expect_true(is(object, "DNAStringSet"))
+    expect_equal(length(object), 365)
+})
+
