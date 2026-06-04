f4552b11859d8f7ce7067f2b4646a83351085553
Hervé Pagès <hpages.on.github@gmail.com>
2026-06-04 08:16:09 -0700

    small tweaks

--- tests/testthat/test-percent_mutation.R
+++ tests/testthat/test-percent_mutation.R
@@ -6,16 +6,23 @@ test_that("percent_mutation()", {
     query <- head(readDNAStringSet(query), n=10L)
     AIRR_df <- igblastn(query)
 
+    EXPECTED_COLNAMES <- c("sequence_id", "locus",
+                           paste0(c("v", "d", "j"), "_perc_mut"))
     for (for.aa in c(FALSE, TRUE)) {
         percent_mut <- percent_mutation(AIRR_df, for.aa=for.aa)
         expect_true(is_tibble(percent_mut))
         expect_identical(dim(percent_mut), c(10L, 5L))
-        expected_colnames <- c("sequence_id", "locus",
-                               paste0(c("v", "d", "j"), "_perc_mut"))
+        expected_colnames <- EXPECTED_COLNAMES
+        if (for.aa)
+            expected_colnames[3:5] <- paste0(expected_colnames[3:5], "_aa")
         expect_identical(colnames(percent_mut), expected_colnames)
         expect_identical(percent_mut$sequence_id, AIRR_df$sequence_id)
         expect_identical(percent_mut$locus, AIRR_df$locus)
-        expect_true(all(is.na(percent_mut$d_perc_mut)))
+        ## Because all the queries are from the light chain, then
+        ## 'AIRR_df$d_call' and all the 'd_*' columns in 'AIRR_df' are
+        ## filled with NAs. So the 'd_perc_mut(_aa)' column in 'percent_mut'
+        ## should also be filled with NAs.
+        expect_true(all(is.na(percent_mut[[4L]])))  # d_perc_mut(_aa) col
         m <- as.matrix(percent_mut[ , tail(expected_colnames, n=3L)])
         expect_true(typeof(m) == "double")
     }
